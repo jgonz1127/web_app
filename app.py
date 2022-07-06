ff6174c37c9474b2d9dc0c3e9288af92d9d1023c
Justin <jgonz1127@fiu.edu>
2022-07-06 14:10:49 -0400

    testi

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template
 import yfinance as yf
 import matplotlib.pyplot as plt
 from io import BytesIO
-
+# TESTING A PUSH
 app = Flask(__name__)
 if __name__=='__main__':
    app.run()
