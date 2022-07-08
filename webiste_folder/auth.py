ceebcf8ab752cf707690eee714c6057527c3b33a
Eduardo L <elagu013@fiu.edu>
2022-07-08 16:06:52 -0400

    added a db and connexted it. Also modified hierarcy and modularized the data requests

---
+++ webiste_folder/auth.py
@@ -0,0 +1,100 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+   #CONNECT to db, ADD account information, ADD followed companies
+
+   #GET company_ticker from search_result.html
+
+   #ADD login functionality with db
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('home'))
+
+        flash(error)
+
+    return render_template('auth/log_in.html')
+
+
+
+#add this for logged in features
+# def login_required(view):
+#     @functools.wraps(view)
+#     def wrapped_view(**kwargs):
+#         if g.user is None:
+#             return redirect(url_for('auth.login'))
+
+#         return view(**kwargs)
+
+#     return wrapped_view
+
+#logout
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+#check if usern is signed in already stored in session available in g resources
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+   #CONNECT with database
+@bp.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == 'POST':
+        username= request.form['username']
+        password = request.form['password']
+        db=get_db()
+        error=None
+        
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+            
+
+    return render_template('auth/sign_up.html')
---
+++ webiste_folder/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS user;
+
+/* change primary key */
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+/* add email  */);
