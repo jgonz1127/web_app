3a3c94ad68c3e1391527db735ea5b38df1b634fa
Eduardo L <elagu013@fiu.edu>
2022-07-24 22:41:16 -0400

    improved run times and reduced codebase via method calls, caching, and asynchronous implemetnation. Home page was also altered.

--- webiste_folder/__init__.py
+++ webiste_folder/__init__.py
@@ -1,7 +1,7 @@
 #used to prevent data from corruption over transfer
 from newsapi import NewsApiClient
 import urllib
-import flask_monitoringdashboard as dashboard
+#import flask_monitoringdashboard as dashboard
 import requests
 from PIL import Image
 from flask_caching import Cache
@@ -15,6 +15,7 @@ from flask import Flask, request, render_template, redirect, url_for
 # from flask.ext.images import resized_img_src
 from sys import stderr
 import matplotlib
+from sqlalchemy import true
 matplotlib.use('Agg') 
 import matplotlib.pyplot as plt
 import os
@@ -27,9 +28,10 @@ from datetime import date
 from datetime import timedelta
 import pandas as pd
 import asyncio
+import math 
+import random 
 
-# API KEY
-newsapi = NewsApiClient(api_key='0dea726db11d4447930182ff2920e22f')
+
 
 # PANDAS DISPLAY
 pd.set_option('display.max_columns', None)
@@ -50,7 +52,7 @@ def create_app(test_config=None):
    cache = Cache(app)
    #debug tool
    toolbar = DebugToolbarExtension(app)
-   dashboard.bind(app)
+   #dashboard.bind(app)
 
     
    if test_config is None:
@@ -73,66 +75,8 @@ def create_app(test_config=None):
       #WILL COMPARE TOP COMAPNIES POST TOP 3 GRAPHS AND INFO ON HOME, make this into one loop and make them all concurrent task calls as to not do sequentially 
       #removed for simplicity, will need ifs and try blocks for none values.
       top_companies = ["TSLA","GOOGL", "MKL", "AMZN",
-                       "BKNG", "NVR", "SEB", "NXGPY", "LDSVF"]
-      #Find company with highest volume
-      async def make_call (company):
-         return yf.Ticker(company)
-         
-      async def homepage_graph_calls():
-         # async with aiohttp.ClientSession() as session:
-         # tasks= create_list()
-         tasks=[]
-         returnList =[]
-         for company in top_companies:
-            task = asyncio.create_task(make_call(company))
-            tasks.append(task)
-         for task in tasks:
-            result = await task
-            returnList.append(result)
-         return returnList
-   
-      start = time()
-      api_responses= asyncio.run(homepage_graph_calls())
-      print(time()-start)
-      # for response in api_responses:   
-      #    string = "name: {} mc: {} volume {}".format(response.info['longName'],response.info['marketCap'],response.info['volume'])
-      #    print(string)
-      @cache.cached(timeout=15000, key_prefix="company_calls")
-      def work():
-         highest_volume_company = api_responses[0]
-         highest_marketCap_company = api_responses[0]
-         for response in api_responses:   
-            if(highest_volume_company.info['volume'] < response.info['volume']):
-               highest_volume_company = response
-               top_company = response
-               
-            if(highest_marketCap_company.info['marketCap'] < response.info['marketCap']):
-               highest_marketCap_company = response
-         return (top_company, highest_marketCap_company, highest_volume_company)
-      print(time()-start)
-      tuple_holder = work()
-      top_company = tuple_holder[0]
-      highest_volume_company = tuple_holder[2]
-      highest_marketCap_company = tuple_holder[1]
-      third_company = "MKL"
-
-      # WILL GRAB IMAGE AND ARTICLE SUMMARY OF 3 ARTICLES AND DISPLAY PREVIEW WITH LINKS TO FULL ARTICLES
-      # TOP "STOCK" NEWS
-      top_headlines = newsapi.get_everything(q='stocks', language='en', page_size=3)
-
-      # Returns list of articles
-      articles = top_headlines['articles']
-
-      #TOP COMPANY DATA 
-      top_ticker_symbol = top_company
-      top_company_name = yf.Ticker(top_ticker_symbol)
+                       "BKNG", "NVR", "SEB", "LDSVF"]
       
-      
-
-      # MAKING articles into data frame allows for easier manipulation
-      articles_df = pd.DataFrame(articles)
-      
-      # print(articles_df)
 
       # ENUMERATE through articles and grabs general information
       article_titles = []
@@ -140,80 +84,125 @@ def create_app(test_config=None):
       article_descriptions = []
       article_images = []
       article_url = []
-      for x in range(0, 3 , 1):
-         article_titles.append(articles_df['title'][x])
-         article_authors.append(articles_df['author'][x])
-         article_descriptions.append(articles_df['description'][x])
-         article_images.append(articles_df['urlToImage'][x])
-         article_url.append(articles_df['url'][x])
+
+      async def populate_article_arrays(x):
+            article_titles.append(articles_df['title'][x])
+            article_authors.append(articles_df['author'][x])
+            article_descriptions.append(articles_df['description'][x])
+            article_images.append(articles_df['urlToImage'][x])
+            article_url.append(articles_df['url'][x])
+      #Find company with highest volume
+      async def make_call (company):
+         return yf.Ticker(company)
+      def article_api_call():
+         global newsapi
+         newsapi =  NewsApiClient(api_key='0dea726db11d4447930182ff2920e22f')
+         global top_headlines
+         top_headlines =  newsapi.get_everything(q='stocks', language='en', page_size=3)
+
+      # Returns list of articles
+         global articles
+         articles =  top_headlines['articles']
+
+      # MAKING articles into data frame allows for easier manipulation
+         global articles_df
+         articles_df=  pd.DataFrame(articles)
+         return True
+         
+      async def homepage_graph_calls():
+         calls_made = article_api_call()
+         article_tasks = []
+         tasks=[]
+         returnList =[]
+         for company in top_companies:
+            task = asyncio.create_task(make_call(company))
+            tasks.append(task) 
+         if calls_made:
+            for i in range(len(articles_df)):
+               article_tasks.append( asyncio.create_task(populate_article_arrays(i)))  
+         for task in tasks:
+            result = await task
+            returnList.append(result)
+         for task in article_tasks:
+            await task
+         return returnList
+   
+      @cache.cached(timeout=15000, key_prefix="api_calls_home")
+      def calls():
+         global api_responses
+         api_responses = asyncio.run(homepage_graph_calls())
       
-     
-      top_company_name = highest_volume_company
-      top_company_market_price = top_company_name.info['regularMarketPrice']
-      top_company_sector = top_company_name.info['sector']
-
-
-      #TOP Historical graph
-      top_history_data = top_company_name.history(period="max")
-      @cache.cached(timeout=15000, key_prefix="graph1")
-      def graph1():
-         plt.plot(top_history_data['High'], color='Red')
-         plt.title(top_company, color='Black')
-         plt.xlabel('Year')
-         plt.ylabel('$ USD')
-         top_buf = BytesIO()
-         plt.savefig(top_buf, format="png")
-         top_data = base64.b64encode(top_buf.getbuffer()).decode("ascii")
-         plt.close()
-         return top_data
-      top_data= graph1()
+      start = time()
+      print(time()-start)
+      calls()
+      print(time()-start)
+      print('calls made! making comparisons')
+         
+      #@TODO pre-cache the dict values for faster look ups
+      @cache.cached(timeout=100, key_prefix="chosen_companys")
+      def work():
+         NUM_HOME_GRAPHS = 3
+         temp_list = []
+         index_list = []
+         #choose a random company
+         for i in range(NUM_HOME_GRAPHS):
+               while True:
+                  index = random.randrange(0,len(top_companies))
+                  if index in index_list: 
+                     continue
+                  index_list.append(index)
+                  temp_list.append(api_responses[index])
+                  break
+         return tuple(temp_list)           
+      tuple_holder = work()
+      print('comparisons done')
+      print(time()-start)
+      comp_1 = tuple_holder[0]
+      comp_2 = tuple_holder[1]
+      comp_3 = tuple_holder[2]
       
-      #HIGHEST MARKET CAP COMPANY DATA
-      second_company_name = highest_marketCap_company
-
-      second_company_market_price = second_company_name.info['regularMarketPrice']
-      second_company_sector = second_company_name.info['sector']
-
-      #SECOND Historical graph
-      second_history_data = second_company_name.history(period="max")
-      @cache.cached(timeout=15000, key_prefix="graph2")
-      def graph2():
-         plt.plot(second_history_data['High'])
-         plt.title(second_company_name, color='Black')
-         plt.xlabel('Year')
-         plt.ylabel('$ USD')
-         second_buf = BytesIO()
-         plt.savefig(second_buf, format="png")
-         second_data = base64.b64encode(second_buf.getbuffer()).decode("ascii")
+      print(time()-start)
+      #returns futures and is coroutine
+      async def make_new_graph(history_time, xlabel, ylabel,company_obj):
+         company = company_obj.history(period=history_time)
+         plt.plot(company['High'], color='Green')
+         plt.title(company_obj.info['longName'], color='Black')
+         plt.xlabel(xlabel)
+         plt.ylabel(ylabel)
+         buffer = BytesIO()
+         plt.savefig(buffer, format="png")
+         data = base64.b64encode(buffer.getbuffer()).decode("ascii")
          plt.close()
-         return second_data
-      second_data = graph2()
-
-      #THIRD COMPANY DATA
-      third_ticker_symbol = third_company
-      third_company_name = yf.Ticker(third_ticker_symbol)
-      third_company_market_price = third_company_name.info['regularMarketPrice']
-      third_company_sector = third_company_name.info['sector']
-
-      #THIRD Historical graph
-      third_history_data = third_company_name.history(period="max")
-      @cache.cached(timeout=15000, key_prefix="graph3")
-      def graph3():
-         plt.plot(third_history_data['High'], color='Green')
-         plt.title(third_company, color='Black')
-         plt.xlabel('Year')
-         plt.ylabel('$ USD')
-         third_buf = BytesIO()
-         plt.savefig(third_buf, format="png")
-         third_data = base64.b64encode(third_buf.getbuffer()).decode("ascii")
-         plt.close()
-         return third_data
-      third_data = graph3()
+         return  data
+  
+      async def create_home_graphs():
+         graphs= []
+         task1 = asyncio.create_task(make_new_graph("1y", "Year", "$ USD", comp_1))
+         task2 = asyncio.create_task(make_new_graph("1y","Year", "$ USD" , comp_2))
+         task3 = asyncio.create_task(make_new_graph("1y", "Year", "$USD", comp_3))
+         
+         graphs.append(await task1)
+         graphs.append(await task2)
+         graphs.append(await task3)
+         
+         return graphs
       
+      #graphs = asyncio.run(create_home_graphs())
+
+      #@TODO make into function to cache the resulsts
+      @cache.cached(timeout=100, key_prefix="chosen_company_graphs")
+      def graphs():
+         graphs = asyncio.run(create_home_graphs())
+         return graphs
+ 
+      graph_list = graphs()
+      top_data = graph_list[0]
+      second_data = graph_list[1]
+      third_data = graph_list[2]
       print(time()-start)
 
       #SEARCH BAR  
-      #home will not redirect to search unless search bar is reused on search page
+     #@TODO FIX home will not redirect to search unless search bar is reused on search page
       if request.method == "POST":
          def render_company_image(url):
             response = requests.get(url)
@@ -265,7 +254,6 @@ def create_app(test_config=None):
             plt.savefig(buf2, format="png")
             data2 = base64.b64encode(buf2.getbuffer()).decode("ascii")
             plt.close()
-            print(history_data)
             
             history_data = company_name.history(period="1d")           
             plt.plot(history_data['High'])
@@ -275,7 +263,6 @@ def create_app(test_config=None):
             plt.savefig(buf3, format="png")
             data3 = base64.b64encode(buf3.getbuffer()).decode("ascii")
             plt.close()
-            print(history_data)
 
             # print("before change")
             # #STOCK CHANGE - Change is the difference between the current price and the last trade of the previous day
@@ -323,21 +310,9 @@ def create_app(test_config=None):
       print(time()-start)
 
       return render_template ('index.html',
-      top_ticker_symbol_to_send = top_company_name,
-      top_company_sector_to_send = top_company_sector,
-      top_company_market_price_to_send = top_company_market_price,
       top_data_to_send = top_data,
-
-      second_ticker_symbol_to_send = second_company_name,
-      second_company_sector_to_send = second_company_sector,
-      second_company_market_price_to_send = second_company_market_price,
       second_data_to_send = second_data,
-
-      third_ticker_symbol_to_send = third_ticker_symbol,
-      third_company_sector_to_send = third_company_sector,
-      third_company_market_price_to_send = third_company_market_price,
       third_data_to_send = third_data,
-
       # ARTICLES
       article_titles_to_send = article_titles,
       article_authors_to_send = article_authors,
@@ -496,3 +471,18 @@ def create_app(test_config=None):
    app.register_blueprint(auth.bp)
       
    return app
+
+        # temp = api_responses[0]
+   # highest_volume_company = [temp, temp.info['volume'], temp.info['marketCap']]
+   # highest_marketCap_company = [temp, temp.info['volume'], temp.info['marketCap']]
+   # cached_list = []
+   # for response in api_responses:
+   #    cached_list.append([response, response.info['volume'], response.info['marketCap']])
+   # for item in cached_list:
+   #    if(highest_volume_company[1] < item[1]):
+   #       highest_volume_company = item
+   #       top_company = item
+
+   #    if(highest_marketCap_company[2] < item[2]):
+   #       highest_marketCap_company = item
+   # return (top_company, highest_marketCap_company, highest_volume_company)
